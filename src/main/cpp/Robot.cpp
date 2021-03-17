4c8b6602438cf19439bb14106c2237a346cbb626
Eric Savage <savage301@gmail.com>
2021-03-17 15:06:58 -0400

    Added paths for intake challenge

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -168,7 +168,7 @@ void Robot::AutonomousPeriodic() {
       // Custom Auto goes here
 
       MyDrive.shift_low();
-      if (count < 1063){
+      if (count < 1113){
         double left_pos = MyPaths.ReturnTableVal(count,0);
         double left_speed = MyPaths.ReturnTableVal(count,1);
         double right_pos = MyPaths.ReturnTableVal(count,2);
